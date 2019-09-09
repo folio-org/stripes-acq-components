96e0e93dd05dca60fcf45042d4153cef364930af
Mikita Siadykh <mikita_siadykh@epam.com>
2019-09-09 11:34:43 +0300

    f

--- test/bigtest/network/configs/users.js
+++ test/bigtest/network/configs/users.js
@@ -1,10 +1,18 @@
+import { Response } from '@bigtest/mirage';
+
 import { USERS_API } from '../../../../lib';
 
 const configUsers = server => {
   server.get(`${USERS_API}/:id`, (schema, request) => {
     const schemaUser = schema.users.find(request.params.id);
 
-    return (schemaUser || {}).attrs;
+    if (!schemaUser) {
+      return new Response(404, {
+        'X-Okapi-Token': `myOkapiToken:${Date.now()}`,
+      }, {});
+    }
+
+    return schemaUser.attrs;
   });
 };
 
