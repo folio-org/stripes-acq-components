63446bb174ed70fd876ac8b0d9a492a1bf1b7ebe
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2019-10-11 13:03:38 +0300

    add MultiSelection interactor

---
+++ test/bigtest/interactors/controls/MultiSelectionInteractor.js
@@ -0,0 +1,26 @@
+import {
+  attribute,
+  clickable,
+  collection,
+  count,
+  fillable,
+  interactor,
+  isPresent,
+  text,
+} from '@bigtest/interactor';
+
+const OptionInteractor = interactor(class OptionInteractor {
+  clickOption = clickable();
+  label = text('li *:first-child');
+  id = attribute('id');
+});
+
+export default interactor(class MultiSelectionInteractor {
+  defaultScope = '[class*=multiSelectContainer---]';
+  controlPresent = isPresent('[class*=multiSelectContainer---]');
+  optionCount = count('[class*=multiSelectOptionList---] li');
+  valueCount = count('[class*=valueChipRoot---]');
+  clickControl = clickable('[class*=multiSelectControl---]');
+  fillFilter = fillable('[class*=multiSelectInput---]');
+  options = collection('[class*=multiSelectOption---]', OptionInteractor);
+});
