b7d68fdc573f6afe5fc229c6e8dc0b992edebf11
Mikita Siadykh <mikita_siadykh@epam.com>
2020-01-24 19:37:50 +0300

    change babel options

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -1,15 +1,13 @@
 module.exports = {
   'presets': [
-    ['@babel/preset-env', {
-      useBuiltIns: 'entry',
-    }],
+    '@babel/preset-env',
     '@babel/preset-react',
   ],
   'plugins': [
     '@babel/plugin-proposal-class-properties',
     [
       '@babel/plugin-proposal-decorators',
-      { decoratorsBeforeExport: true },
+      { decoratorsBeforeExport: false },
     ],
     '@babel/plugin-transform-runtime',
   ],
