46ee2211fa8cd1e75474a2a99e955b5fb69eb3a5
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-02-26 13:35:54 +0300

    add checkbox interactor

--- test/bigtest/interactors/controls/index.js
+++ test/bigtest/interactors/controls/index.js
@@ -1,4 +1,5 @@
 export { default as ButtonInteractor } from './ButtonInteractor';
+export { default as CheckboxInteractor } from './CheckboxInteractor';
 export { default as MultiSelectionInteractor } from './MultiSelectionInteractor';
 export { default as OptionListInteractor } from './OptionListInteractor';
 export { default as SelectInteractor } from './SelectInteractor';
