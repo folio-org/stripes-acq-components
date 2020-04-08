0c8dcb8da144866cdf8d1fc0808191fc50d9be41
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-04-08 14:18:13 +0300

    fix tests

--- test/jest/__mock__/stripesCore.mock.js
+++ test/jest/__mock__/stripesCore.mock.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 jest.mock('@folio/stripes/core', () => ({
   ...require.requireActual('@folio/stripes/core'),
-  stripesConnect: Component => props => <Component {...props} mutator={{}} />,
+  stripesConnect: Component => props => <Component {...props} />,
 
   // eslint-disable-next-line react/prop-types
   Pluggable: props => <>{props.children}</>,
