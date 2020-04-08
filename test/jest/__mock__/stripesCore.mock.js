734a9583b368cb75e3f86b93f8a46081e85d14ae
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-04-08 13:46:15 +0300

    UIOR-516 PluggableUserFilter component

--- test/jest/__mock__/stripesCore.mock.js
+++ test/jest/__mock__/stripesCore.mock.js
@@ -2,5 +2,8 @@ import React from 'react';
 
 jest.mock('@folio/stripes/core', () => ({
   ...require.requireActual('@folio/stripes/core'),
-  stripesConnect: Component => props => <Component {...props} />,
+  stripesConnect: Component => props => <Component {...props} mutator={{}} />,
+
+  // eslint-disable-next-line react/prop-types
+  Pluggable: props => <>{props.children}</>,
 }), { virtual: true });
