fc04ae92f3781ad2494953833294c053e5562263
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-09-09 20:40:19 +0300

    UIORGS-201 Update view pane to display null as hyphen (#338)

--- test/jest/__mock__/stripesCore.mock.js
+++ test/jest/__mock__/stripesCore.mock.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 jest.mock('@folio/stripes/core', () => ({
   ...jest.requireActual('@folio/stripes/core'),
-  stripesConnect: Component => props => <Component {...props} />,
+  stripesConnect: Component => ({ mutator, ...rest }) => {
+    const fakeMutator = mutator || Object.keys(Component.manifest).reduce((acc, mutatorName) => {
+      acc[mutatorName] = {
+        GET: jest.fn(),
+        PUT: jest.fn(),
+        POST: jest.fn(),
+        DELETE: jest.fn(),
+        reset: jest.fn(),
+      };
+
+      return acc;
+    }, {});
+
+    return <Component {...rest} mutator={fakeMutator} />;
+  },
 
   // eslint-disable-next-line react/prop-types
   Pluggable: props => <>{props.children}</>,
