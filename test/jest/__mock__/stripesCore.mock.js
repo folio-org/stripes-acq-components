30ee0f5c61444979991df44dd05cfd30ecf560de
Mikita Siadykh <mikita_siadykh@epam.com>
2021-02-10 11:54:42 +0400

    Update stripesCore.mock.js

--- test/jest/__mock__/stripesCore.mock.js
+++ test/jest/__mock__/stripesCore.mock.js
@@ -70,7 +70,7 @@ jest.mock('@folio/stripes/core', () => {
 
       return <Component {...rest} mutator={fakeMutator} resources={fakeResources} stripes={fakeStripes} />;
     },
-    
+
     useOkapiKy: jest.fn(),
 
     useStripes: () => STRIPES,
