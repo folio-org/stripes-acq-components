c279270881bd5573d4cf3216085b651df1ef7fea
Alex Mozolëv <72964370+axelhunn@users.noreply.github.com>
2021-05-13 13:39:10 +0300

    UISACQCOMP-31 Update jest-related configs (#493)
    * add ["@babel/plugin-proposal-private-methods", \{ "loose": true }] to Babel plugins (due to Babel's warning)
    * add TS transformation
    * add config metadata mock

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -2,10 +2,12 @@ module.exports = {
   'presets': [
     '@babel/preset-env',
     '@babel/preset-react',
+    '@babel/preset-typescript',
   ],
   'plugins': [
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+    ['@babel/plugin-proposal-private-methods', { 'loose': true }],
     '@babel/plugin-transform-runtime',
   ],
 };
