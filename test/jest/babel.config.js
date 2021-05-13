fa1a2de46f50607973e6ab84f2be9b2f61a90139
Alex Mozolev <aleksey_mozolev@epam.com>
2021-05-13 11:54:08 +0300

    fix code style

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -7,7 +7,7 @@ module.exports = {
   'plugins': [
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ['@babel/plugin-proposal-private-methods', { 'loose': true }],
     '@babel/plugin-transform-runtime',
   ],
 };
