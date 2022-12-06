a7d1ccb636337936597256b80aa4b08656cebd58
Yury Saukou <yury_saukou@epam.com>
2022-12-06 09:39:15 +0400

    add audit events fixtures

--- test/jest/fixtures/index.js
+++ test/jest/fixtures/index.js
@@ -4,5 +4,7 @@ export * from './batchGroup';
 export * from './invoice';
 export * from './invoiceExportReport';
 export * from './invoiceLine';
+export * from './orderAuditEvent';
 export * from './orderLine';
+export * from './orderLineAuditEvent';
 export * from './vendor';
