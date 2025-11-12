45e2995ea11195db1994134a0e88d8a094871967
Yury Saukou <yury_saukou@epam.com>
2025-11-12 12:42:10 +0400

    get coverage from the experimental folder

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ const config = require('@folio/jest-config-stripes');
 
 module.exports = {
   ...config,
+  collectCoverageFrom: [
+    ...config.collectCoverageFrom,
+    `${path.join(__dirname, './experimental')}/**/*.{js,jsx}`,
+  ],
   setupFiles: [
     ...config.setupFiles,
     path.join(__dirname, './test/jest/setupFiles.js'),
